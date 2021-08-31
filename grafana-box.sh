a74a6cdec36504ef005f05233067807d405764c2
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2021-08-30 22:12:48 -1000

    more cli parsing work

--- grafana-box.sh
+++ grafana-box.sh
@@ -18,12 +18,13 @@
 ###################################################
 
 
-usage() { echo -e "Usage: $0 [-d <distro>] [-w <workflow>] [-p <processor>]\n" 1>&2; exit 1; }
+usage() { echo -e "Usage: $0 [-d <distro>] [-w <workflow>] [optional: -a <FLAG_ONLY> runs AMD processors instead of default Intel]\n" 1>&2; exit 1; }
 
-while getopts ":d:w:p:" o; do
+while getopts ":d:w: :a" o; do
     case "${o}" in
         d)
-            d=${OPTARG}            
+            d=${OPTARG}
+            #pattern-match the argument against a list in a file           
             if ! [[ "$d" =~ $(echo ^\($(paste -sd'|' distro-list)\)$) ]]; then
               echo -e "You have not chosen a valid distro.\nPlease choose one from the following list:\n"
               cat distro-list
@@ -42,10 +43,6 @@ while getopts ":d:w:p:" o; do
               IMAGE_FAMILY=centos-cloud
             elif [[ ${DISTRO} =~ windows ]]; then
               IMAGE_FAMILY=windows-cloud
-            # else
-            #   echo "You have not chosen a valid os. Please choose your parameters from the following list:"
-            #   cat distro-list
-            #   # usage
             fi
             ;;
         w)
@@ -53,23 +50,20 @@ while getopts ":d:w:p:" o; do
             if ! [[ "$w" =~ ^(devenv|package|[0-9]\.[0-9]\.[0-9]+)$ ]]; then
               echo -e "You have not chosen a valid workflow.\nPlease choose one from the following list:\n\n* package (if available, uses native package manager)\n* devenv  (fresh build from main branch. Grafana Frontend (yarn start) and Backend (make run) launched in detached Tmux sessions)\n* version (enter as 3 digits. -w 7.5.10, for example, will install Grafana version 7.5.10)\n"              
               usage
-            else
+            elif [[ "$w" =~ ^[0-9]\.[0-9]\.[0-9]+$ ]]; then
+              WORKFLOW=binary
+              GF_VERSION=${w}
+              CPU_COUNT=2
+            elif [[ ${w} =~ ^devenv$ ]]; then
               WORKFLOW=${w}
-            fi
-            
-            if [[ ${WORKFLOW} =~ ^devenv$ ]]; then
               CPU_COUNT=8
             else
+              WORKFLOW=${w}
               CPU_COUNT=2
             fi
             ;;
-        p)
-            p=${OPTARG}
-            if [[ "$p" =~ ^amd$ ]]; then
-              MACHINE_TYPE=n2d
-            else
-              MACHINE_TYPE=e2
-            fi
+        a)
+            MACHINE_TYPE=n2d
             ;;
         *)
             usage
@@ -78,16 +72,24 @@ while getopts ":d:w:p:" o; do
 done
 shift $((OPTIND-1))
 
+# these two parameters can't be null
 if [ -z "${d}" ] || [ -z "${w}" ]; then
   usage
 fi
 
-echo -e "all fields validated\nbuilding Terraform plan..."
-echo machine_type is ${MACHINE_TYPE}
-echo cpu_count is ${CPU_COUNT}
-echo image_family is ${IMAGE_FAMILY}
-echo image_project is ${DISTRO}
-echo workflow is ${WORKFLOW}
+# workaround to keep the -a flag optional but set a default when it's absent
+if ! [[ ${MACHINE_TYPE} == n2d ]]; then
+  MACHINE_TYPE=e2
+fi
+
+echo -e "\nall fields validated\n"
+echo -e "machine_type ====> ${MACHINE_TYPE}"
+echo "cpu_count =======> ${CPU_COUNT}"
+echo "image_family ====> ${IMAGE_FAMILY}"
+echo "image_project ===> ${DISTRO}"
+echo -e "workflow ========> ${WORKFLOW}\n"
+echo -e "building Terraform plan...\n"
+
 
 # create new 'terraform.tfvars' file with injected vars
 cat <<EOT > gcp/terraform.tfvars
@@ -114,8 +116,8 @@ sudo apt-get upgrade -y
 sudo apt-get install -y adduser libfontconfig1 wget
 
 # get binary (not the standalone)
-wget https://dl.grafana.com/oss/release/grafana_${GRAFANA_BOX_VERSION}_amd64.deb
-sudo dpkg -i grafana_${GRAFANA_BOX_VERSION}_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_${GF_VERSION}_amd64.deb
+sudo dpkg -i grafana_${GF_VERSION}_amd64.deb
 
 # start and add to systemd
 sudo /bin/systemctl daemon-reload
@@ -124,5 +126,5 @@ sudo /bin/systemctl start grafana-server
 EOT
 
 # kick off terraform build
-# cd gcp/
-# terraform apply
+cd gcp/
+terraform apply
