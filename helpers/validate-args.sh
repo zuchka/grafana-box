39181d2329d7054c720d4d87b4b1d1fe58fca61d
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2021-09-09 22:37:44 -1000

    adds destroy command

--- helpers/validate-args.sh
+++ helpers/validate-args.sh
@@ -1,72 +1,73 @@
 #!/bin/bash
 
 # getops arg-parsing logic
+function usage() { echo -e "Usage: $0 [-d <distro>] [-w <workflow>] [optional: -a <FLAG_ONLY> runs AMD processors instead of default Intel]\n" 1>&2; exit 1; }
+
 function validateArgs () {
     case "${o}" in
         d)
-            d=${OPTARG}
+            d="${OPTARG}"
             #pattern-match the argument against a list in a file           
-            if ! [[ "$d" =~ $(echo ^\($(paste -sd'|' ./helpers/distro-list)\)$) ]]; then
-            echo -e "You have not chosen a valid distro.\nPlease choose one from the following list:\n"
-            cat ./helpers/distro-list
+            if ! [[ "${d}" =~ $(echo ^\($(paste -sd'|' ./helpers/distro-list)\)$) ]]; then
+                echo -e "You have not chosen a valid distro.\nPlease choose one from the following list:\n"
+                cat ./helpers/distro-list
             echo -e "\n"              
-            usage
+                usage
             else
-            DISTRO=${d}
+                DISTRO="${d}"
             fi
             
-            if [[ ${DISTRO} =~ ubuntu ]]; then
-            IMAGE_FAMILY=ubuntu-os-cloud
-            elif [[ ${DISTRO} =~ debian ]]; then
-            IMAGE_FAMILY=debian-cloud
-            elif [[ ${DISTRO} =~ centos ]]; then
-            IMAGE_FAMILY=centos-cloud
-            elif [[ ${DISTRO} =~ rocky ]]; then
-            IMAGE_FAMILY=rocky-linux-cloud
-            elif [[ ${DISTRO} =~ windows ]]; then
-            IMAGE_FAMILY=windows-cloud          
+            if [[ "${DISTRO}" =~ ubuntu ]]; then
+                IMAGE_FAMILY="ubuntu-os-cloud"
+            elif [[ "${DISTRO}" =~ debian ]]; then
+                IMAGE_FAMILY="debian-cloud"
+            elif [[ "${DISTRO}" =~ centos ]]; then
+                IMAGE_FAMILY="centos-cloud"
+            elif [[ "${DISTRO}" =~ rocky ]]; then
+                IMAGE_FAMILY="rocky-linux-cloud"
+            elif [[ "${DISTRO}" =~ windows ]]; then
+                IMAGE_FAMILY="windows-cloud"         
             fi
             ;;
         w)
-            w=${OPTARG}
-            if ! [[ "$w" =~ ^(devenv|package|[0-9]\.[0-9]\.[0-9]+) ]]; then
-            echo -e "You have not chosen a valid workflow.\nPlease choose one from the following list:\n\n* package (if available, uses native package manager)\n* devenv  (fresh build from main branch. Grafana Frontend (yarn start) and Backend (make run) launched in detached Tmux sessions)\n* version (enter as 3 digits. -w 7.5.10, for example, will install Grafana version 7.5.10)\n"              
-            usage
-            elif [[ "$w" =~ ^[0-9]\.[0-9]\.[0-9]+$ ]]; then
-            WORKFLOW=binary
-            GF_VERSION=${w}
-            CPU_COUNT=2
-            elif [[ ${w} =~ ^devenv ]]; then
-            WORKFLOW=devenv
-            BRANCH=${w}
-            CPU_COUNT=8
+            w="${OPTARG}"
+            if ! [[ "${w}" =~ ^(devenv|package|[0-9]\.[0-9]\.[0-9]+) ]]; then
+                echo -e "You have not chosen a valid workflow.\nPlease choose one from the following list:\n\n* package (if available, uses native package manager)\n* devenv  (fresh build from main branch. Grafana Frontend (yarn start) and Backend (make run) launched in detached Tmux sessions)\n* version (enter as 3 digits. -w 7.5.10, for example, will install Grafana version 7.5.10)\n"              
+                usage
+            elif [[ "${w}" =~ ^[0-9]\.[0-9]\.[0-9]+$ ]]; then
+                WORKFLOW="binary"
+                GF_VERSION="${w}"
+                CPU_COUNT=2
+            elif [[ "${w}" =~ ^devenv ]]; then
+                WORKFLOW="devenv"
+                BRANCH="${w}"
+                CPU_COUNT=8
             else
-            WORKFLOW=${w}
-            CPU_COUNT=2
+                WORKFLOW="${w}"
+                CPU_COUNT=2
             fi
             ;;
         a)
-            MACHINE_TYPE=n2d
+            MACHINE_TYPE="n2d"
             ;;
         *)
             usage
             ;;
     esac
-
-    shift "$((OPTIND-1))"
-
 }
 
 function validateBranch () {
-    if [[ ${BRANCH} =~ ^devenv$ ]]; then
-        BRANCH=main
+    if [ -z "${BRANCH}" ]; then
+        return
+    elif [[ "${BRANCH}" =~ ^devenv$ ]]; then
+        BRANCH="main"
     else
-        # drop first seven characters of ${w}
+        # drop first seven characters of ${w} "devenv-"
         BRANCH=$(echo "${BRANCH}" | cut -c 8-)
         echo "checking existence of remote branch '${BRANCH}'"
         
         BRANCH_VAL=$(git ls-remote --heads git@github.com:grafana/grafana.git "${BRANCH}" | wc -l)
-        # if one, continue. else, break with message: "remote branch ${foo} does not exist"
+
         if [[ "${BRANCH_VAL}" == 0 ]]; then
             echo -e "\n${BRANCH} is not a valid remote branch. please try again\n"
             usage
@@ -83,10 +84,12 @@ function nullCheck () {
     fi
 
     # workaround to keep the -a flag optional but set a default when it's absent
-    if ! [[ ${MACHINE_TYPE} == n2d ]]; then
-        MACHINE_TYPE=e2
+    if ! [[ "${MACHINE_TYPE}" =~ ^n2d$ ]]; then
+        MACHINE_TYPE="e2"
     fi
 
+    # use printf instead
+    # make separate function; report this with ip at end? and ram?
     echo -e "\nall fields validated\n"
     echo -e "machine_type ====> ${MACHINE_TYPE}"
     echo "cpu_count =======> ${CPU_COUNT}"
