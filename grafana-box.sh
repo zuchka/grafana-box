509d9470ecbd95ade6132cc95b6ee42d06d755df
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2021-09-14 15:03:54 -1000

    refactor init steps

--- grafana-box.sh
+++ grafana-box.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# shellcheck disable=SC1090
+
 # run init script to copy template & set directory variables 
 # OR destroy boxes on "destroy"
-. ./init.sh
+. init.sh
 
 # source helper functions AFTER init script
-for helper in ./helpers/*.sh; do
+for helper in helpers/*.sh; do
   . "${helper}"
 done
 
-
 # validate args
 while getopts ":d:w: :a :n:" o; do
-  validateArgs
+  validateArgs "${o}"
 done
 
 shift "$((OPTIND-1))"
@@ -21,20 +22,16 @@ shift "$((OPTIND-1))"
 validateBranch
 nullCheck
 
-# and then change destroy logic to search recursively through dir and find all plans
-
 # generate provisioning scripts and terraform.tfvars
-printValues
 makeBinary
 makePackage
 makeDevenv
 makeTfvars
 
 # kick off terraform build
-terraform -chdir=${GFB_FOLDER}/ init
-terraform -chdir=${GFB_FOLDER}/ apply
+terraform -chdir="${GFB_FOLDER}"/ init
+terraform -chdir="${GFB_FOLDER}"/ apply
 # terraform -chdir=${GFB_FOLDER}/ show
 
 # print the VM ip + metadata
-MACHINE_IP=$(terraform -chdir=${GFB_FOLDER}/ output -raw instance_ip)
 printValues
--- init.sh
+++ init.sh
@@ -5,10 +5,12 @@
 if [[ "${1}" =~ ^destroy$ ]]; then
   GFB_FOLDERS=$(find . -type f -iname "main.tf" | sed '/gcp/d' | sed s/main\.tf//g)
   while IFS= read -r line; do
-    terraform -chdir=$line/ destroy -auto-approve
+    terraform -chdir="$line"/ destroy -auto-approve
   done <<< "${GFB_FOLDERS}"
   exit
 else 
   GFB_FOLDER=$(date +%s)
-  cp -r ./gcp ./${GFB_FOLDER}
+  cp -r ./gcp ./"${GFB_FOLDER}"
 fi
+
+# add ./grafana-box.sh delete to remove folders with warning
